b89842e526ce6ca54fe8e0008f14315210a6c7f5
Rahul-Harakari <rmh42742@gmail.com>
2024-04-08 22:12:53 +0530

    Implemented crossbrowser parallel testing

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -13,8 +13,11 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;  //Log4j
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	
@@ -24,10 +27,23 @@ public Logger logger;
 	
 	
 	@BeforeClass
-	public void setup() {
+	@Parameters({"os", "browser"}
+			)
+	public void setup(String os, String br) {
 		
 		
 		logger = LogManager.getLogger(this.getClass());
+		
+		switch(br.toLowerCase()){
+		case "chrome" : driver=new ChromeDriver(); break;
+		case "firefox" : driver=new FirefoxDriver(); break;
+		case "edge" : driver= new EdgeDriver(); break;
+		default : System.out.println("No matching browser found");
+		return; // Exit from the script in case no browser is selected
+		
+		}
+		
+		
 		
 		driver=new ChromeDriver();
 		driver.manage().deleteAllCookies();
